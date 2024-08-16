f75a9da500cceb035e494fe4435d027bf108c248
Yoshihiro Kuroki <yoshihiro.kuroki@gmail.com>
2024-08-16 23:42:12 +0900

    modifying the paths to the dependent modules

--- src/PMParameterized.jl
+++ src/PMParameterized.jl
@@ -1,7 +1,7 @@
 # PMarameterized MetaPackage
 module PMParameterized
 using Reexport
-@reexport using PMParameterizedBase
-@reexport using PMParameterizedSolve
+@reexport using ..PMParameterizedBase
+@reexport using ..PMParameterizedSolve
 # @reexport using PMParameterizedSensitivity
 end
